07d1bd5f8b151d96ec8e515db47e3e13844c8d47
given-woo <2210220@bsis.hs.kr>
2022-07-06 22:16:46 +0900

    Initail Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,140 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import "./App.css";
+
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA-8PTyCOvK-bLW5Zml2ocAqtALl5UsEuI",
+    authDomain: "chatapp-7d3f5.firebaseapp.com",
+    projectId: "chatapp-7d3f5",
+    storageBucket: "chatapp-7d3f5.appspot.com",
+    messagingSenderId: "835391524528",
+    appId: "1:835391524528:web:32d09bcc9740887e46b575",
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+} else {
+    firebase.app();
+}
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [user] = useAuthState(auth);
+
+    return (
+        <div className="App">
+            <header>
+                <h1>HUMAN ALLY</h1>
+                <SignOut />
+            </header>
+
+            <section>{user ? <ChatRoom /> : <SignIn />}</section>
+        </div>
+    );
+}
+
+function SignIn() {
+    const signInWithGoogle = () => {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider);
+    };
+
+    return (
+        <>
+            <button className="sign-in" onClick={signInWithGoogle}>
+                Sign in with Google
+            </button>
+        </>
+    );
+}
+
+function SignOut() {
+    return (
+        auth.currentUser && (
+            <button className="sign-out" onClick={() => auth.signOut()}>
+                Sign Out
+            </button>
+        )
+    );
+}
+
+function ChatRoom() {
+    const dummy = useRef();
+    const messagesRef = firestore.collection("messages");
+    const query = messagesRef.orderBy("createdAt");
+
+    const [messages] = useCollectionData(query, { idField: "id" });
+
+    const [formValue, setFormValue] = useState("");
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+
+        const { uid, photoURL } = auth.currentUser;
+
+        await messagesRef.add({
+            text: formValue,
+            createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+            uid,
+            photoURL,
+        });
+
+        setFormValue("");
+        dummy.current.scrollIntoView({ behavior: "smooth" });
+    };
+
+    return (
+        <>
+            <main>
+                {messages &&
+                    messages.map((msg) => (
+                        <ChatMessage key={msg.id} message={msg} />
+                    ))}
+
+                <span ref={dummy}></span>
+            </main>
+
+            <form onSubmit={sendMessage}>
+                <input
+                    value={formValue}
+                    onChange={(e) => setFormValue(e.target.value)}
+                    placeholder="Type Your message here!"
+                />
+
+                <button type="submit" disabled={!formValue}>
+                    Send
+                </button>
+            </form>
+        </>
+    );
+}
+
+function ChatMessage(props) {
+    const { text, uid, photoURL } = props.message;
+
+    const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
+
+    return (
+        <>
+            <div className={`message ${messageClass}`}>
+                <img
+                    src={
+                        photoURL ||
+                        "https://i1.wp.com/jejuhydrofarms.com/wp-content/uploads/2020/05/blank-profile-picture-973460_1280.png?ssl=1"
+                    }
+                />
+                <p>{text}</p>
+            </div>
+        </>
+    );
 }
 
 export default App;
